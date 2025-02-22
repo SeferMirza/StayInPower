d32d6004144c6074b697bc9c9efa5fcbbb40b5ef
Sefer Mirza <sefermirza@gmail.com>
2025-02-23 00:05:44 +0300

    remove unnecessary files

--- src/StayInPower/eslint.config.js
+++ src/StayInPower/eslint.config.js
@@ -17,12 +17,12 @@ export default [
 
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginPlaywright.configs['flat/recommended'],
     files: ['e2e/**/*.{test,spec}.{js,ts,jsx,tsx}'],
