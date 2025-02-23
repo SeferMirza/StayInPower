ac67d0f4f07a7066db81326c1d0613695c8885e2
Sefer Mirza <sefermirza@gmail.com>
2025-02-23 21:20:07 +0300

    rename folder and test demo

---
+++ src/app/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import cardsData from '@/story-test.json';
+
+console.log(cardsData);
+</script>
+
+<template>
+  <header>
+    <div>
+      States
+    </div>
+  </header>
+
+  <div class="content-block">
+    <div class="cards">
+      cards
+    </div>
+    <div>
+      right side events
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.content-block {
+  width: 100%;
+  height: 100%;
+  align-content: center;
+  display: flex;
+  flex-direction: row;
+}
+
+.cards {
+  display: flex;
+  justify-content: center;
+}
+
+header {
+  width: 100%;
+}
+</style>
---
+++ src/app/src/components/Card.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>image</div>
+    <div>name</div>
+    <div>detail</div>
+    <div>stat</div>
+  </div>
+</template>
